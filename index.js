0c9cef1cd4d2b3aa2321df4c0c47771eb44a0620
ryankissoon <ryankissoon_31@msn.com>
2022-01-15 02:31:19 -0500

    commit tree 673f179624615ece8c0a924a781a45fa07406ded

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+  if (someValue < 400 ){
+    return ('This one is on me!');
+  }else if (someValue > 2000 && someValue < 2500){
+    return ('I will gladly take your thirty bucks.');
+  }else if (someValue >= 2500){
+    return ('No can do.');
+  }  
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(someLocation){
   // Write your code here!
+  return someLocation === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  switch(someTip){
+    case 'generous':
+      return 'Thank you so much.'
+      break
+    case 'not as generous':
+      return 'Thank you.'
+      break
+    default:
+      return 'Bye.'  
+  }
 }
